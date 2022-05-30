038489191e3d749cb5a89e989de9992abe4a8094
RodneyMac <rodneydavidmaciel@gmail.com>
2022-05-30 12:46:21 -0300

    first commit

---
+++ src/screen/ProfileView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Products from '../components/Products/Products';
+
+const ProfileView = () => {
+  return (
+    <>
+      <Products/>
+    </>
+  )
+}
+
+export default ProfileView;
